7b91a77349dd3f08491149b20e6e600690383277
aVALKY <Fillierelouis1@gmail.com>
2024-04-17 13:58:02 +0200

    affichage de tout les pokemon de la db avec une structure css

--- index.php
+++ index.php
@@ -4,29 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PokeAPI</title>
+    <link rel="stylesheet" href="style.css">
 </head>
-<body>
-    <h1>Pokemon</h1>
 
-    <table>
-        <thead>
-            <th>img_poke</th>
-            <th>Nom</th>
-            <th>Taille</th>
-            <th>Description</th>
-            <th>HP</th>
-            <th>Attack</th>
-            <th>Defence</th>
-            <th>Vitesse</th>
-            <th>Attack_spe</th>
-            <th>Defence_spe</th>
-            <th>Generation</th>
-            <th>Version</th>
-            <th>Habitat</th>
-            <th>Compétence</th>
-            <th>Catégorie</th>
-        </thead>
-    <tbody>
+<body>
+    <div id="containerImgTitle">
+        <img id="imgTitle" src="assets/pokemonpng.png" alt="imgPokemon">
+    </div>
+
+    <div id="containerPokemon">
         <?php
 
             require  'database.php';
@@ -34,27 +20,25 @@
             $appeleDeLaFunctionGetPokemon = Database::getPokemon();
 
             while ($poke = $appeleDeLaFunctionGetPokemon->fetch()) {
-              
-                echo'<td>'.$poke['nom'].'</td>';
-                echo'<td>';
-                echo'<td>'.$poke['taille'].'</td>';
-                echo'<td>'.$poke['description'].'</td>';
-                echo'<td>'.$poke['hp'].'</td>';
-                echo'<td>'.$poke['attack'].'</td>';
-                echo'<td>'.$poke['defence'].'</td>';
-                echo'<td>'.$poke['vitesse'].'</td>';
-                echo'<td>'.$poke['attack_spe'].'</td>';
-                echo'<td>'.$poke['defence_spe'].'</td>';
-                echo'<td>'.$poke['generation'].'</td>';
-                echo'<td>'.$poke['img_poke'].'</td>';
-                echo'<td>';
-                echo'</td>';
+                echo'<div id="containerImgPokemon"><img src = "assets/images/'.$poke['img_poke'].'"></div>';
+                
+                
+                // echo'<td>'.$poke['nom'].'</td>';
+                // echo'<td>'.$poke['taille'].'</td>';
+                // echo'<td>'.$poke['description'].'</td>';
+                // echo'<td>'.$poke['hp'].'</td>';
+                // echo'<td>'.$poke['attack'].'</td>';
+                // echo'<td>'.$poke['defence'].'</td>';
+                // echo'<td>'.$poke['vitesse'].'</td>';
+                // echo'<td>'.$poke['attack_spe'].'</td>';
+                // echo'<td>'.$poke['defence_spe'].'</td>';
+                // echo'<td>'.$poke['generation'].'</td>';
+                // echo'<td>'.$poke['img_poke'].'</td>';
+                // echo'<td>';
+                // echo'</td>';
             }
 
         ?>
-    </tbody>
-</table>
-
-
+    </div>
 </body>
 </html>
